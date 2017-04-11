3c4a8eb81479d8c873d8aee9a6728eb361b579e0
hadyRashwan <h2rashwan@gmail.com>
2017-04-11 15:40:25 +0200

    adding 2 blocks version
    the multi blocks version is created statically can be improved in the future

---
+++ ipsative_Two_Static_Blocks/ipsative.js
@@ -0,0 +1,27 @@
+testRunner.controllerProvider.register("questionnaire", function($scope) {
+  $scope.responseOptions = {{options}};
+  $scope.questions = {{questions}}
+  var questionsOverTwo=($scope.questions.length/2)+1 ;
+  $scope.questionsBlockOne=$scope.questions.slice(1,questionsOverTwo);
+  $scope.questionsBlockTwo=$scope.questions.slice(questionsOverTwo,$scope.questions.length);
+// ipsative function here
+$scope.ipsetiveButtonChecker = function(ipsetiveQuestionID, ipsetiveQuestionAnswer,tableQuestionID) {
+    var tableQuestionArray=document.getElementById(tableQuestionID).getElementsByTagName('input')
+    var tableQuestionArrayNames=[];
+    for(var i=0 ; i<tableQuestionArray.length ; i++){
+      tableQuestionArrayNames.push(tableQuestionArray[i].name)
+    }
+    $scope.questions.forEach(function(item, index, itemsArray) {
+        if (item.answer == ipsetiveQuestionAnswer && item.id != ipsetiveQuestionID && tableQuestionArrayNames.indexOf("q" + item.id)!= -1 ) {
+            document.getElementsByName("q" + item.id).checked = false
+            item.answer = null;
+        }
+    })
+}
+///
+});
+// ng-click="ipsetiveButtonChecker(question.id,option.value,'upperTable')" // add this part
+
+// to this line
+// <td class="questionnaire-response-cell" ng-repeat="option in responseOptions"><input name="q[[question.id]]" ng-click="ipsetiveButtonChecker(question.id,option.value)" ng-model="question.answer" type="radio" value="[[option.value]]" /></td>
+// also add a name to the table
