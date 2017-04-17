787dacee966f54d5b7a1c2cc3e4f437400eb0892
hadyRashwan <h2rashwan@gmail.com>
2017-04-17 17:43:03 +0200

    concept of check_ipsative_questions_answered

--- ipsative_Button_checker.js
+++ ipsative_Button_checker.js
@@ -1,12 +1,13 @@
 // just a concept
-var numberOfTables = document.getElementById(tableQuestionID).getElementsByTagName('table').length
-var numberOfAnswers = 0;
-$scope.questions.forEach(function(item, index, itemsArray) {
-    if (item.answer != null)
-        numberOfAnswers = numberOfAnswers + 1
-
-})
-if(numberOfAnswers!=numberOfTables*2){ //TODO to be changed to number of responses instead of just 2
-// prevent the next question
-
+$scope.ipsative_button_next=function(){
+  var numberOfTables = document.getElementById('testContainer').getElementsByTagName('table').length
+  var numberOfAnswers = 0
+  $scope.questions.forEach(function(item, index, itemsArray) {
+      if (item.answer != null)
+          numberOfAnswers = numberOfAnswers + 1
+  })
+  if( numberOfAnswers != numberOfTables*$scope.responseOptions.length){ // TODO to be changed to number of responses instead of just 2
+  // prevent the next question
+  alert("you missed a question")
+  }
 }
