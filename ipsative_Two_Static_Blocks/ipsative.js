10d6c7a5d8d080aea086339832309a0b5f10ec5c
Hady Rashwan <h2rashwan@gmail.com>
2017-05-30 19:52:08 +0200

    update js file for version 2.186

--- ipsative_Two_Static_Blocks/ipsative.js
+++ ipsative_Two_Static_Blocks/ipsative.js
@@ -1,22 +1,35 @@
 testRunner.controllerProvider.register("questionnaire", function($scope) {
   $scope.responseOptions = {{options}};
   $scope.questions = {{questions}}
+  //document.getElementsByName("next")[0].disabled=true
+
   var questionsOverTwo=($scope.questions.length/2) ;
   $scope.questionsBlockOne=$scope.questions.slice(0,questionsOverTwo);
   $scope.questionsBlockTwo=$scope.questions.slice(questionsOverTwo,$scope.questions.length);
 // ipsative function here
 $scope.ipsetiveButtonChecker = function(ipsetiveQuestionID, ipsetiveQuestionAnswer,tableQuestionID) {
+    var numberOfTables = document.getElementById('testContainer').getElementsByTagName('table').length
+    var numberOfAnswers = 0
     var tableQuestionArray=document.getElementById(tableQuestionID).getElementsByTagName('input')
     var tableQuestionArrayNames=[];
     for(var i=0 ; i<tableQuestionArray.length ; i++){
       tableQuestionArrayNames.push(tableQuestionArray[i].name)
     }
     $scope.questions.forEach(function(item, index, itemsArray) {
+        if (typeof item.answer =="string")
+          numberOfAnswers = numberOfAnswers + 1
         if (item.answer == ipsetiveQuestionAnswer && item.id != ipsetiveQuestionID && tableQuestionArrayNames.indexOf("q" + item.id)!= -1 ) {
+          numberOfAnswers=numberOfAnswers-1
             document.getElementsByName("q" + item.id).checked = false
             item.answer = null;
         }
+          
     })
+      if( numberOfAnswers == numberOfTables*$scope.responseOptions.length){ 
+  // prevent the next question
+   //document.getElementsByName("next")[0].disabled=false
+  //alert("you missed a question")
+  }
 }
 ///
 });
