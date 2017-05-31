045d675b667de59d23aef2a2f06ecfe1310791ab
Hady Rashwan <h2rashwan@gmail.com>
2017-05-31 10:01:35 +0200

    Update ipsative.js

--- ipsative_Two_Static_Blocks/ipsative.js
+++ ipsative_Two_Static_Blocks/ipsative.js
@@ -1,37 +1,56 @@
 testRunner.controllerProvider.register("questionnaire", function($scope) {
-  $scope.responseOptions = {{options}};
-  $scope.questions = {{questions}}
-  //document.getElementsByName("next")[0].disabled=true
+  $scope.responseOptions = {
+    {
+      options
+    }
+  }; // responseOptions e.g agree or disagree
+  $scope.questions = {
+    {
+      questions
+    }
+  } // the questions e.g do support liverpool ?
+  var numberOfTables = document.getElementById('testContainer').getElementsByTagName('table').length // count the tables
+  var numberOfAnswers = 0 // set the counter of answers to zero at first 
+  document.getElementsByName("next")[0].disabled = true // disable the next button at first
+  $scope.questions.forEach(function(item, index, itemsArray) { // a for each to count the number of answers
+    if (typeof item.answer == "string")
+      numberOfAnswers = numberOfAnswers + 1
+  })
+  if (numberOfAnswers == numberOfTables * $scope.responseOptions.length) { // see if all answers are collected
+    // prevent the next question
+    document.getElementsByName("next")[0].disabled = false // open the button
 
-  var questionsOverTwo=($scope.questions.length/2) ;
-  $scope.questionsBlockOne=$scope.questions.slice(0,questionsOverTwo);
-  $scope.questionsBlockTwo=$scope.questions.slice(questionsOverTwo,$scope.questions.length);
-// ipsative function here
-$scope.ipsetiveButtonChecker = function(ipsetiveQuestionID, ipsetiveQuestionAnswer,tableQuestionID) {
-    var numberOfTables = document.getElementById('testContainer').getElementsByTagName('table').length
-    var numberOfAnswers = 0
-    var tableQuestionArray=document.getElementById(tableQuestionID).getElementsByTagName('input')
-    var tableQuestionArrayNames=[];
-    for(var i=0 ; i<tableQuestionArray.length ; i++){
+  }
+  var questionsOverTwo = ($scope.questions.length / 2); // divid the questions to 2 tables for two blocks preview
+  $scope.questionsBlockOne = $scope.questions.slice(0, questionsOverTwo); // the first part
+  $scope.questionsBlockTwo = $scope.questions.slice(questionsOverTwo, $scope.questions.length); // second part
+  // ipsative function here
+  $scope.ipsetiveButtonChecker = function(ipsetiveQuestionID, ipsetiveQuestionAnswer, tableQuestionID) { // when a answer is choosen make a  ipsetive check
+    numberOfAnswers = 0 // rest the counter to zero
+    var tableQuestionArray = document.getElementById(tableQuestionID).getElementsByTagName('input') //get to know  which block is the exam excuted from
+    var tableQuestionArrayNames = []; // a empty array for questions 
+    for (var i = 0; i < tableQuestionArray.length; i++) {
       tableQuestionArrayNames.push(tableQuestionArray[i].name)
     }
     $scope.questions.forEach(function(item, index, itemsArray) {
-        if (typeof item.answer =="string")
-          numberOfAnswers = numberOfAnswers + 1
-        if (item.answer == ipsetiveQuestionAnswer && item.id != ipsetiveQuestionID && tableQuestionArrayNames.indexOf("q" + item.id)!= -1 ) {
-          numberOfAnswers=numberOfAnswers-1
-            document.getElementsByName("q" + item.id).checked = false
-            item.answer = null;
-        }
-          
+      if (typeof item.answer == "string") // count the number of answers
+        numberOfAnswers = numberOfAnswers + 1 // +ve add answers
+      if (item.answer == ipsetiveQuestionAnswer && item.id != ipsetiveQuestionID && tableQuestionArrayNames.indexOf("q" + item.id) != -1) { // check if the answer is already answered
+        numberOfAnswers = numberOfAnswers - 1 // -ve one of the ansers
+        document.getElementsByName("q" + item.id).checked = false // remove the check 
+        item.answer = null; // make the value null
+      }
+
     })
-      if( numberOfAnswers == numberOfTables*$scope.responseOptions.length){ 
-  // prevent the next question
-   //document.getElementsByName("next")[0].disabled=false
-  //alert("you missed a question")
+    if (numberOfAnswers == numberOfTables * $scope.responseOptions.length) { // check if they did answer everything
+      // prevent the next question
+      document.getElementsByName("next")[0].disabled = false //remove the disable
+      //alert("you missed a question")
+    } else {
+      document.getElementsByName("next")[0].disabled = true //re enable the function
+    }
   }
-}
-///
+  ///
 });
 // ng-click="ipsetiveButtonChecker(question.id,option.value,'upperTable')" // add this part
 
